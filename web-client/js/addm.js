9a780bcb26a98c07caf622fef5dfb51eea53e7ae
Kegsay <kegsay@gmail.com>
2014-06-27 16:38:52 +0100

    Add days to uptime to make it less misleading.

--- web-client/js/addm.js
+++ web-client/js/addm.js
@@ -2,13 +2,14 @@
 function msToTime(duration) {
     var seconds = parseInt((duration/1000)%60)
         , minutes = parseInt((duration/(1000*60))%60)
-        , hours = parseInt((duration/(1000*60*60))%24);
+        , hours = parseInt((duration/(1000*60*60))%24)
+        , days = parseInt((duration/(1000*60*60*24)));
 
     hours = (hours < 10) ? "0" + hours : hours;
     minutes = (minutes < 10) ? "0" + minutes : minutes;
     seconds = (seconds < 10) ? "0" + seconds : seconds;
 
-    return hours + "h " + minutes + "m " + seconds + "s";
+    return (days > 0 ? days+"d " : "") + hours + "h " + minutes + "m " + seconds + "s";
 }
 
 $(window).load(function(){
