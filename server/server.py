d3ed5a1060ab6c85a664a24adb2f264f03d6b258
Kegsay <kegsay@gmail.com>
2014-06-27 13:51:51 +0100

    Path full db path to web.py

--- server/server.py
+++ server/server.py
@@ -57,9 +57,10 @@ def setup_db():
 
     script_folder = os.sep.join(os.path.realpath(__file__).split(os.sep)[:-1]) # snip off filename
     data_folder = os.path.join(script_folder, "data") # subdir of script folder
-    con = sqlite3.connect(os.path.join(data_folder, dbname))
+    database_abs_path = os.path.join(data_folder, dbname)
+    con = sqlite3.connect(database_abs_path)
     con.execute('CREATE TABLE IF NOT EXISTS devices(id TEXT PRIMARY KEY, data TEXT, UNIQUE(id) ON CONFLICT REPLACE);')
-    return web.database(dbn='sqlite', db=dbname)
+    return web.database(dbn='sqlite', db=database_abs_path)
         
 def main():
     db = setup_db()
